56af9f96260ee0787a4c9a3f71a6af5974a7ef51
user <ashirokov.exceedteam@gmail.com>
2021-10-13 12:22:50 +0300

    Get all visits

--- src/modules/controllers/visitControllers.js
+++ src/modules/controllers/visitControllers.js
@@ -5,30 +5,3 @@ module.exports.getAllVisits = (req, res, next) => {
 		res.send({data: result});
 	});
 };
-
-module.exports.createNewVisit = (req, res) => {
-	const visit = Visit(req.body);	
-	visit.save().then(result => {
-		res.send({data: result});
-	});
-};
-
-module.exports.changeVisitInfo = (req, res) => {
-	Visit.updateOne({_id: req.body._id}, req.body).then(result => {
-		Visit.find().then(result => {
-			res.send({data: result});
-		});
-	});
-};
-
-module.exports.deleteVisit = (req, res) => {
-	if (req.query._id) {
-		Visit.deleteOne({_id: req.query._id}).then(result => {
-			Visit.find().then(result => {
-				res.send({data: result});
-			});
-		});	
-	} else {
-		res.status(422).send('Error! Params not correct');
-	}
-};
