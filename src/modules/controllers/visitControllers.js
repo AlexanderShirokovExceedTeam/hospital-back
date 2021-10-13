751cfbe29b4fa37c1f2dabb281d202705700a88b
user <ashirokov.exceedteam@gmail.com>
2021-10-13 17:33:55 +0300

    Change visit info

--- src/modules/controllers/visitControllers.js
+++ src/modules/controllers/visitControllers.js
@@ -19,16 +19,30 @@ module.exports.createNewVisit = (req, res) => {
 	}
 };
 
+module.exports.changeVisitInfo = (req, res) => {
+	if (reqBodyIsValid(req.body)) {
+		Visit.updateOne({_id: req.body._id}, req.body).then(result => {
+			Visit.find().then(result => {
+				res.send({data: result});
+			});
+		});
+	} else {
+		res.status(422).send({
+			message: 'Error! Fill all fields!'
+		});
+	}
+};
+
 const reqBodyIsValid = (reqBody) => {	
 	if (reqBody.hasOwnProperty('patient')
-	&& reqBody.hasOwnProperty('doctor')
-	&& reqBody.hasOwnProperty('date')
-	&& reqBody.hasOwnProperty('problem')) {
-		const { patient, doctor, date, problem } = reqBody;		
-			if ( patient
-				&& doctor
-				&& date
-				&& problem) {
+			&& reqBody.hasOwnProperty('doctor')
+			&& reqBody.hasOwnProperty('date')
+			&& reqBody.hasOwnProperty('problem')) {
+		const { patient, doctor, date, problem } = reqBody;
+			if (patient
+					&& doctor
+					&& date
+					&& problem) {
 				return true;
 			} else {
 				return false;
