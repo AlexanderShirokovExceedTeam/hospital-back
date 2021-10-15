90265c71f9700645dc578d5935beff2132b84f90
user <ashirokov.exceedteam@gmail.com>
2021-10-15 16:28:02 +0300

    JWT authorizasion, middleware, fixed visitControllers

--- src/modules/controllers/visitControllers.js
+++ src/modules/controllers/visitControllers.js
@@ -2,10 +2,8 @@ const Visit = require('../../db/models/visits');
 const jwt = require('jsonwebtoken');
 const key = require('../keys/keys');
 
-module.exports.getAllVisits = async (req, res, next) => {
-	const { token } = req.headers;
-	const decodedToken = jwt.verify(token, key.jwt);
-	const idFromToken = { userId: decodedToken._id };
+module.exports.getAllVisits = async (req, res) => {
+	const idFromToken = { userId: req.user._id };
 
 	Visit.find(idFromToken).then(result => {
 		res.send({data: result});
@@ -16,13 +14,11 @@ module.exports.createNewVisit = async (req, res) => {
 	const allFields = true;
 	
 	if (reqBodyIsValid(req.body, allFields)) {
-		const { token } = req.headers;
-		const decodedToken = jwt.verify(token, key.jwt);
-		req.body.userId = decodedToken._id;		
+		req.body.userId = req.user._id;
 		const visit = new Visit(req.body);
 
 		visit.save().then(result => {
-			Visit.find({ userId: decodedToken._id }).then(result => {
+			Visit.find({ userId: req.user._id }).then(result => {
 				res.send({ data: result });
 			});
 		});
@@ -35,13 +31,10 @@ module.exports.createNewVisit = async (req, res) => {
 
 module.exports.changeVisitInfo = async (req, res) => {
 	const noAllFields = false;
-	
-	if (reqBodyIsValid(req.body, noAllFields)) {
-		const { token } = req.headers;
-		const decodedToken = jwt.verify(token, key.jwt);
 
+	if (reqBodyIsValid(req.body, noAllFields)) {
 		Visit.updateOne({_id: req.body._id}, req.body).then(result => {
-			Visit.find({ userId: decodedToken._id }).then(result => {
+			Visit.find({ userId: req.user._id }).then(result => {
 				res.send({data: result});
 			});
 		});
@@ -54,11 +47,8 @@ module.exports.changeVisitInfo = async (req, res) => {
 
 module.exports.deleteVisit = async (req, res) => {
 	if (req.query._id) {
-		const { token } = req.headers;
-		const decodedToken = jwt.verify(token, key.jwt);
-
 		Visit.deleteOne({_id: req.query._id}).then(result => {
-			Visit.find({ userId: decodedToken._id }).then(result => {
+			Visit.find({ userId: req.user._id }).then(result => {
 				res.send({data: result});
 			});
 		});
